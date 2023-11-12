93833bb8c909492c47bff2208e264764cb9a7fe4
Ricardo Oliveira <ricardo@hashicorp.com>
2023-11-12 17:25:45 +0000

    trees need to start on the top

--- cmd/create-playoffs.go
+++ cmd/create-playoffs.go
@@ -135,8 +135,8 @@ func (o *createPlayoffOptions) run(cmd *cobra.Command, args []string) error {
 
 		depth := helper.CalculateDepth(subtrees[i])
 		fmt.Printf("With tree Depth: %d\n", depth)
-		helper.PrintLeafNodes(subtrees[i], f, subtreeSheet, depth*2, 2, depth, false)
-		helper.PrintLeafNodes(subtrees[i], f, subtreeSheet, depth*2, 2, depth, false)
+		helper.PrintLeafNodes(subtrees[i], f, subtreeSheet, depth*2, 0, depth, false)
+		helper.PrintLeafNodes(subtrees[i], f, subtreeSheet, depth*2, 0, depth, false)
 	}
 	f.DeleteSheet("Tree")
 	if err != nil {

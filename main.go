c6c37d43d96734926124e172f1fd4e3fbc8ecc09
Ricardo Oliveira <ricardo@hashicorp.com>
2023-12-11 01:09:25 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/gitrgoliveira/bracket-creator/cmd"
+import (
+	"embed"
+
+	"github.com/gitrgoliveira/bracket-creator/cmd"
+	"github.com/gitrgoliveira/bracket-creator/internal/helper"
+)
+
+//go:embed web/*
+var webFiles embed.FS
+
+//go:embed template.xlsx
+var templateFile embed.FS
 
 func main() {
+	helper.WebFs = webFiles
+	helper.TemplateFile = templateFile
 	cmd.Execute()
 }
